e320d8d55e8a0757715b46f157c0809db01987b4
Mat(2.718)i <75086843+matei-george@users.noreply.github.com>
2022-11-01 13:56:13 +0200

    Update FabricaCutii.java

--- Laboratoare/Laborator_4/src/magazin/FabricaCutii.java
+++ Laboratoare/Laborator_4/src/magazin/FabricaCutii.java
@@ -4,36 +4,7 @@ import cutii.TipCutie;
 import cutii.iCutie;
 import jucarii.Jucarie;
 
-public class FabricaCutii extends Jucarie implements iCutie {
-
-    public FabricaCutii(double l1, double l2, double l3) {
-        super(l1, l2, l3);
-    }
-
-    @Override
-    public TipCutie getTipCutie() {
-        return null;
-    }
-
-    @Override
-    public double getPret() {
-        return 0;
-    }
-
-    @Override
-    public double getSuprafataTotala() {
-        return 0;
-    }
-
-    @Override
-    public double getLungimePanglica() {
-        return 0;
-    }
-
-    @Override
-    public double pret() {
-        return iCutie.super.pret();
-    }
+public class FabricaCutii {
     public void getCutie(Jucarie jucarie){
         if(jucarie.getTipCutie()==TipCutie.PARALELIPIPED)
             jucarie.getTipCutie();
